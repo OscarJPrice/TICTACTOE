a78d840ae034d31489ee279cc4ec136173f28f29
Oscar Price <43356609+The-Subpar-Scholar@users.noreply.github.com>
2022-10-13 01:42:51 -0700

    Update Board.py
    Minor Style Changes

--- Board.py
+++ Board.py
@@ -19,17 +19,17 @@ class GameBoard:
     def __init__(self):
         self._data = [ [0 for i in range(self.SIZE)] for i in range(self.SIZE)]
     
-    def get(self, x:int, y:int)->str:
+    def get(self, x: int, y: int) -> str:
         return self._data[self.SIZE-y][x-1]
 
-    def set(self, x:int, y:int, piece:int)->bool:
+    def set(self, x: int, y: int, piece: int) -> bool:
         if x>self.SIZE or y>self.SIZE: return False
-        if x<1 or y<1: return False
+        elif x<1 or y<1: return False
         elif self.get(x, y) != 0: return False
         self._data[self.SIZE-y][x-1] = piece
         return True
 
-    def show(self)->None:
+    def show(self) -> None:
         add_padd = lambda n, m: ' '*(int(log10(m))-int(log10(n))) + str(n)
         print(' ' * int(log10(self.SIZE)) + 'Y')
         for y in range(self.SIZE):
@@ -44,14 +44,14 @@ class GameBoard:
             print(f' {x+1}' + ' '*(3-int(log10(self.SIZE))-int(log10(x+1)) ), end='')
         print(' X')
     
-    def clear(self)->None:
+    def clear(self) -> None:
         system('cls' if name == 'nt' else 'clear')
 
-    def reset(self)->None:
+    def reset(self) -> None:
         self.__init__(self)
         clear()
 
-    def eval(self)->None:
+    def eval(self) -> None:
         win = lambda x: -1 if x == -self.SIZE else 1 if x == self.SIZE else 0
         game_over = True
         for y in self._data:
