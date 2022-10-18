091e2d8a47d26d4f1095b2b31e222e75948a9fce
The-Subpar-Scholar <fizzpope@gmail.com>
2022-10-17 17:29:54 -0700

    Change input scheme to keyboard (arrow/wasd) bases

--- Board.py
+++ Board.py
@@ -1,61 +1,86 @@
 from os import system, name
-from math import log10
-x = 1
-o = -1
-_ = 0
+from keyboard import is_pressed
+from time import sleep
+
 IMPASSE = -9999
-PIECES_DICT = {
-    1 : 'x',
-    -1 : 'o',
-    0 : '_',
+PIECES = {
+    1  : 'x',   'x' :  1,
+    -1 : 'o',   'o' : -1,
+    0  : '_',   '_' :  0,
     IMPASSE : 'none'
 }
+turn = 1
 
 class GameBoard:
-    SIZE = 3
     _data = []
+    _selection_x, _selection_y = 0, 0
+    INPUT_SLEEP = 0.1
+    SIZE = 6
 
+    fore_message = ''
+    aft_message = ''
+
+    
+
+    def _cartesian_to_arr_indexes(self, x: int, y: int):
+        return x-1, self.SIZE-y
     
     def __init__(self):
-        self._data = [ [0 for i in range(self.SIZE)] for i in range(self.SIZE)]
+        """use a generator expression to initialize each square for board of size SIZE to the char '_' """
+        self._data = [ [PIECES['_'] for i in range(self.SIZE)] for i in range(self.SIZE)]
     
-    def get(self, x: int, y: int) -> str:
-        return self._data[self.SIZE-y][x-1]
+    def get(self, x: int, y: int, is_cartesian: bool = True) -> str:
+        """returns the selected square from the board interpreted as a cartesian coordinate plane, with the square closest to the origin being (1, 1)"""
+        if is_cartesian: x, y = self._cartesian_to_arr_indexes(x, y)
+        return self._data[y][x]
 
-    def set(self, x: int, y: int, piece: int) -> bool:
-        if x>self.SIZE or y>self.SIZE: return False
-        elif x<1 or y<1: return False
-        elif self.get(x, y) != 0: return False
-        self._data[self.SIZE-y][x-1] = piece
+    def set(self, x: int, y: int, piece: int, is_cartesian: bool = True) -> bool:
+        """sets a piece on the board according to cartesian interpretation of the board"""
+        
+        if is_cartesian: x, y = self._cartesian_to_arr_indexes(x, y)
+
+        if ( 
+            (x>self.SIZE or y>self.SIZE) or # a given coordinate is too big
+            (x<0 or y<0) or # a given coordinate is too small 
+            (self.get(x, y, False) != 0) # the space is occupied
+            ): return False
+        
+        self._data[y][x] = piece
         return True
 
     def show(self) -> None:
-        add_padd = lambda n, m: ' '*(int(log10(m))-int(log10(n))) + str(n)
-        print(' ' * int(log10(self.SIZE)) + 'Y')
+        """Outputs the board"""
+        olen = lambda n=self.SIZE: len(str(n)) # Python has no builtin to find the length of an integer's digits
+        add_padding = lambda n, m=self.SIZE: ' '*( olen(m) - olen(n) ) + str(n) # adds padding corresponding to the olen(larger_n) - olen(n)
+        square_is_selected = lambda x, y: y == self._selection_y and x == self._selection_x
+
+        print(self.fore_message)#add_padding('Y'))
         for y in range(self.SIZE):
-            print(f"{add_padd(self.SIZE-y, self.SIZE)} ", end='')
+            print(f"{ add_padding(self.SIZE - y) } ", end='')
+
             for x in range(self.SIZE):
-                print(f"_{PIECES_DICT[self._data[y][x]]}_", end='')
-                if x==self.SIZE-1: break
-                print('|', end='')
+                print( f"{'[' if square_is_selected(x, y) else ' '}{ PIECES[ self._data[y][x] ]}", end=']' if square_is_selected(x, y) else ' ')
+
             print()
-        print("  ", end='')
-        for x in range(self.SIZE):
-            print(f' {x+1}' + ' '*(3-int(log10(self.SIZE))-int(log10(x+1)) ), end='')
-        print(' X')
+        print(self.aft_message)
     
+
     def clear(self) -> None:
         system('cls' if name == 'nt' else 'clear')
 
     def reset(self) -> None:
         self.__init__(self)
-        clear()
+        self.clear()
+
+    def refresh(self) -> None:
+        self.clear()
+        self.show()
 
     def eval(self) -> None:
         win = lambda x: -1 if x == -self.SIZE else 1 if x == self.SIZE else 0
         game_over = True
         for y in self._data:
-            if _ in y: game_over = False
+            if 0 in y: game_over = False
             state = win(sum(y)) 
             if(state!=0): return state
         for x in range(self.SIZE):
@@ -77,3 +102,31 @@ class GameBoard:
         if(state!=0): return state
         if(game_over): return IMPASSE
         return 0
+
+    def get_user_selection(self) -> tuple[int, int]:
+
+        while not is_pressed('enter'):
+            if is_pressed('q'): quit()
+            
+            if ( is_pressed("a") or is_pressed("left_arrow") ) and self._selection_x - 1 >= 0:
+                self._selection_x -= 1
+                self.refresh()
+                sleep(self.INPUT_SLEEP)
+
+            elif ( is_pressed("d") or is_pressed("right_arrow") ) and self._selection_x + 1 < self.SIZE:
+                self._selection_x += 1
+                self.refresh()
+                sleep(self.INPUT_SLEEP)
+
+            if ( is_pressed("s") or is_pressed("down_arrow") ) and self._selection_y + 1 < self.SIZE:
+                self._selection_y += 1
+                self.refresh()
+                sleep(self.INPUT_SLEEP)
+
+            elif ( is_pressed("w") or is_pressed("up_arrow") ) and self._selection_y - 1 >= 0:
+                self._selection_y -= 1
+                self.refresh()
+                sleep(self.INPUT_SLEEP)
+
+        sleep(self.INPUT_SLEEP)
+        return self._selection_x, self._selection_y
--- tictactoe.py
+++ tictactoe.py
@@ -1,19 +1,19 @@
-from Board import GameBoard, PIECES_DICT, x, o
+from Board import GameBoard, PIECES, turn
 gb = GameBoard()
-turn = 1
 round_res = 0
-while(1):
-    gb.clear()
-    gb.show()
-    inp = input(f"It is {PIECES_DICT[turn]}'s turn, where would you like to place your piece? x y: ").strip().split(' ')
-    if inp[0] == 'q': quit()
-    if len(inp) != 2: continue
-    if(not (inp[0]+inp[1]).isdigit()): continue
-    x, y = int(inp[0]), int(inp[1])
-    if(not gb.set(x, y, turn)): continue
+while(round_res==0):
+    gb.fore_message = f"It is {PIECES[turn]}'s turn"
+    gb.refresh()
+
+
+    while(True):
+        x, y = gb.get_user_selection()
+        if gb.set(x, y, turn, False): break
+
     round_res = gb.eval()
     if(round_res!=0): break
     turn = -turn
+
 gb.clear()
 gb.show()
-print(f"winner: {PIECES_DICT[round_res]}")
+print(f"winner: {PIECES[round_res]}")
